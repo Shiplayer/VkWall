c480b396295f6835d996c779785ca1ea14602fff
Shiplayer <shiplayer@yandex.ru>
2018-12-03 17:43:49 +0300

    add documentation, modify some logic, modify MainActivity

--- app/src/main/java/com/developer/java/yandex/vkwall/MainActivity.kt
+++ app/src/main/java/com/developer/java/yandex/vkwall/MainActivity.kt
@@ -1,33 +1,79 @@
 package com.developer.java.yandex.vkwall
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.support.design.widget.Snackbar
+import android.support.v4.widget.NestedScrollView
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
 import android.view.View
-import com.developer.java.yandex.vkwall.R.id.*
 import com.developer.java.yandex.vkwall.adapter.VkWallAdapter
 import com.developer.java.yandex.vkwall.entity.VkWallEntity
 import com.developer.java.yandex.vkwall.model.WallModel
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
+
 class MainActivity : AppCompatActivity() {
+    /**
+     * Используется для уведомления о том, что приложение уничтожается и нужно освободить ресурсы
+     */
+    interface DestroyListener {
+        fun onDestroy()
+    }
 
     companion object {
-        public val token = "a10b7fd7a10b7fd7a10b7fd715a144c62baa10ba10b7fd7fafe18ad246c4fcef5af6a9f"
+        /**
+         * Токен для получения данных от vk api
+         */
+        const val TOKEN = "a10b7fd7a10b7fd7a10b7fd715a144c62baa10ba10b7fd7fafe18ad246c4fcef5af6a9f"
+
+        /**
+         * Ключи, по которым сохраняем состояния и восстанавливаем
+         */
         private const val LIST_ITEM_KEY: String = "listItemKey"
         private const val TV_ID_TEXT: String = "textViewIdText"
         private const val TV_COUNT_TEXT: String = "textViewCountText"
         private const val POSITION_SCROLL_VIEW: String = "positionScrollView"
+        private const val FIND_WALL_ID: String = "findWallId"
+        private const val FIND_WALL_COUNT: String = "findWallCount"
+        private const val TOTAL_WALL_POST_COUNT: String = "totalWallPostCount"
+        private const val SHOW_EMPTY_WALL: String = "showEmptyWall"
     }
 
+    /**
+     * Адаптер, который содержит элементы записей на странице
+     */
     private lateinit var mAdapter: VkWallAdapter
+
+    /**
+     * Модель, с помощью которое осуществлено взаимодействие с сервером для отправки запросов
+     */
     private lateinit var mModel: WallModel
-    private val disposable = CompositeDisposable()
+
+    private val mDisposable = CompositeDisposable()
+
+    /**
+     * Общее количество постов
+     */
+    private var mTotalWallPost: Int? = null
+
+    /**
+     * Айди или короткое имя пользователя или сообщества
+     */
+    private var mWallId: String? = null
+
+    /**
+     * Количество, которое необходимо получить
+     */
+    private var mWallCount: String? = null
 
     override fun onSaveInstanceState(outState: Bundle?) {
         outState?.run {
@@ -38,24 +84,45 @@ class MainActivity : AppCompatActivity() {
             array[0] = main_scroll_view.scrollX
             array[1] = main_scroll_view.scrollY
             putIntArray(POSITION_SCROLL_VIEW, array)
+            if (mWallId != null) {
+                putString(FIND_WALL_ID, mWallId)
+            }
+            if (mWallCount != null) {
+                putString(FIND_WALL_COUNT, mWallCount)
+            }
+            if (mTotalWallPost != null) {
+                putInt(TOTAL_WALL_POST_COUNT, mTotalWallPost!!)
+            }
+
+            putBoolean(SHOW_EMPTY_WALL, cb_show_empty.isChecked)
         }
         super.onSaveInstanceState(outState)
 
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
-        savedInstanceState?.run{
+        savedInstanceState?.run {
             edit_id.setText(this.getString(TV_ID_TEXT, ""))
             edit_count.setText(this.getString(TV_COUNT_TEXT, ""))
+            if (this.containsKey(FIND_WALL_ID)) {
+                mWallId = this.getString(FIND_WALL_ID)
+            }
+            if (this.containsKey(FIND_WALL_COUNT)) {
+                mWallCount = this.getString(FIND_WALL_COUNT)
+            }
+            if (this.containsKey(TOTAL_WALL_POST_COUNT)) {
+                mTotalWallPost = getInt(TOTAL_WALL_POST_COUNT)
+            }
+            cb_show_empty.isChecked = getBoolean(SHOW_EMPTY_WALL, false)
         }
         val list = savedInstanceState?.getParcelableArrayList<VkWallEntity>(LIST_ITEM_KEY)?.toList()
-        if(list == null) {
-            mAdapter.setData(listOf())
+        if (list == null) {
+            mAdapter.setData(listOf(), cb_show_empty.isChecked)
         } else {
-            mAdapter.setData(list)
+            mAdapter.setData(list, cb_show_empty.isChecked)
             edit_id.clearFocus()
             edit_count.clearFocus()
-            savedInstanceState?.run {
+            savedInstanceState.run {
                 val array = this.getIntArray(POSITION_SCROLL_VIEW)
                 main_scroll_view.scrollX = array[0]
                 main_scroll_view.scrollY = array[1]
@@ -73,87 +140,174 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    /**
+     * Инициализация обработчиков событий
+     */
     private fun createHandlers() {
-        button.setOnClickListener {
+
+        cb_show_empty.setOnCheckedChangeListener { compoundButton, checked ->
+            mAdapter.setFilter(checked)
+        }
+
+        btn_clear.setOnClickListener {
+            cb_show_empty.isChecked = false
+            mAdapter.setData(listOf(), false)
+            edit_id.setText("")
+            edit_count.setText("")
+            mWallId = null
+            mWallCount = null
+            mTotalWallPost = null
+            tv_total_count.text = ""
+        }
+
+        main_scroll_view.setOnScrollChangeListener { v: NestedScrollView?, scrollX: Int, scrollY: Int, oldScrollX: Int, oldScrollY: Int ->
+            if (v!!.getChildAt(v.childCount - 1) != null && rv_wall.layoutManager.childCount > 0) {
+                if ((scrollY >= (v.getChildAt(v.childCount - 1).measuredHeight - v.measuredHeight)) && scrollY > oldScrollY) {
+                    val visibleItemCount = rv_wall.layoutManager.childCount
+                    val totalItemCount = rv_wall.layoutManager.itemCount
+                    val pastVisibleCount = if (rv_wall.layoutManager is LinearLayoutManager) {
+                        (rv_wall.layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
+                    } else {
+                        (rv_wall.layoutManager as GridLayoutManager).findFirstVisibleItemPosition()
+                    }
+
+                    if (mTotalWallPost != null && mTotalWallPost!! > 0)
+                        if ((visibleItemCount + pastVisibleCount) >= totalItemCount && mWallId != null && mWallCount != null) {
+                            mModel.loadWallMore(mWallId!!, mWallCount!!, mAdapter.getOriginItemCount())
+                        }
+                }
+            }
+        }
+
+        /*button.setOnClickListener {
             mModel.loadWallMore(edit_id.text.toString(), edit_count.text.toString(), mAdapter.itemCount)
 
-        }
+        }*/
 
         btn_show.setOnClickListener {
+            mTotalWallPost = null
+            mWallId = edit_id.text.toString()
+            mWallCount = edit_count.text.toString()
             showLoading()
-            mModel.loadWall(edit_id.text.toString(), edit_count.text.toString())
+            mModel.loadWall(mWallId!!, mWallCount!!)
         }
 
-        disposable.addAll(
-            mModel.getWallHandler()
-                .subscribeOn(Schedulers.io())
-                .map { json ->
-                    mModel.parseResponse(json)
-                }
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({ response ->
-                    if (response.error.isEmpty()) {
-                        mAdapter.setData(response.list)
-                        hiddenLoader()
-                    }
-                    else {
-                        showError()
-                        tv_error_msg.text = response.error
-                    }
-                }, {
-
-                }),
-            mModel.getWallMoreHandler()
-                .subscribeOn(Schedulers.io())
-                .map { json ->
-                    mModel.parseResponse(json)
-                }
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { response ->
-                    if (response.error.isEmpty()) {
-                        mAdapter.addData(response.list)
-                        rv_wall.visibility = View.VISIBLE
-                    }
-                    else{
-                        showError()
-                        tv_error_msg.text = response.error
-                    }
-                }
+        mDisposable.addAll(
+            createWallHandler(),
+            createWallMoreHandler()
         )
     }
 
-    private fun showError(){
+    /**
+     * Создание наблюдателя за получением ответа от сервера для получения еще большего количества постов
+     */
+    private fun createWallMoreHandler(): Disposable {
+        return mModel.getWallMoreHandler()
+            .subscribeOn(Schedulers.io())
+            .map { json ->
+                val response = mModel.parseResponse(json)
+                response
+            }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { response ->
+                if (response.error.isEmpty()) {
+                    mAdapter.addData(response.list, cb_show_empty.isChecked)
+                    rv_wall.visibility = View.VISIBLE
+                } else {
+                    Snackbar.make(rv_wall, response.error, Snackbar.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    /**
+     * Создание наблюдателя за получением ответа от сервера для получения постов
+     */
+    @SuppressLint("SetTextI18n")
+    private fun createWallHandler(): Disposable {
+        return mModel.getWallHandler()
+            .subscribeOn(Schedulers.io())
+            .map { json ->
+                val response = mModel.parseResponse(json)
+                response
+            }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ response ->
+                if (mTotalWallPost == null || mTotalWallPost == -1) {
+                    mTotalWallPost = response.total
+                    tv_total_count.text = resources.getString(R.string.total_count) + " " + response.total
+                }
+                if (response.error.isEmpty()) {
+                    mAdapter.setData(response.list, cb_show_empty.isChecked)
+                    hiddenLoader()
+                } else {
+                    showError()
+                    tv_error_msg.text = response.error
+                }
+            }, {
+
+            })
+    }
+
+    /**
+     * Для получения названия разрешения экрана
+     * @param context - необходим для получения разрешения экрана
+     * @return строка, в которой содержится название разрешения экрана
+     */
+    private fun getSizeName(context: Context): String {
+        var screenLayout = context.resources.configuration.screenLayout
+        screenLayout = screenLayout and Configuration.SCREENLAYOUT_SIZE_MASK
+
+        return when (screenLayout) {
+            Configuration.SCREENLAYOUT_SIZE_SMALL -> "small"
+            Configuration.SCREENLAYOUT_SIZE_NORMAL -> "normal"
+            Configuration.SCREENLAYOUT_SIZE_LARGE -> "large"
+            4 // Configuration.SCREENLAYOUT_SIZE_XLARGE is API >= 9
+            -> "xlarge"
+            else -> "undefined"
+        }
+    }
+
+    /**
+     * Показать ошибку, если она произошла
+     */
+    private fun showError() {
         rv_wall.visibility = View.GONE
         tv_error_msg.visibility = View.VISIBLE
         pb_loading.visibility = View.GONE
     }
 
-    private fun hiddenLoader(){
+    /**
+     * Скрыть прогресс бар и показать RecyclerView после загрузки постов
+     */
+    private fun hiddenLoader() {
         rv_wall.visibility = View.VISIBLE
         tv_error_msg.visibility = View.GONE
         pb_loading.visibility = View.GONE
     }
 
-    private fun showLoading(){
+    /**
+     * Показать прогресс бар, во время загрузки постов
+     */
+    private fun showLoading() {
         rv_wall.visibility = View.GONE
         tv_error_msg.visibility = View.GONE
         pb_loading.visibility = View.VISIBLE
     }
 
-    private fun initLayout(){
-        rv_wall.layoutManager = LinearLayoutManager(baseContext)
+    private fun initLayout() {
+        if (getSizeName(baseContext).contains("large")) {
+            rv_wall.layoutManager = GridLayoutManager(baseContext, 2)
+        } else {
+            rv_wall.layoutManager = LinearLayoutManager(baseContext)
+        }
         rv_wall.isNestedScrollingEnabled = false
         mModel = WallModel()
         mAdapter = VkWallAdapter(mModel)
         rv_wall.adapter = mAdapter
     }
 
-    interface DestroyListener{
-        fun onDestroy()
-    }
-
     override fun onDestroy() {
         super.onDestroy()
-        disposable.dispose()
+        mDisposable.dispose()
     }
 }
--- app/src/main/java/com/developer/java/yandex/vkwall/model/WallModel.kt
+++ app/src/main/java/com/developer/java/yandex/vkwall/model/WallModel.kt
@@ -9,6 +9,7 @@ import com.developer.java.yandex.vkwall.interactors.UserInteractor
 import com.developer.java.yandex.vkwall.retrofit.VkApiService
 import com.google.gson.JsonElement
 import com.google.gson.JsonObject
+import io.reactivex.Notification
 import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -16,7 +17,6 @@ import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.PublishSubject
-import retrofit2.Response
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.Executors
@@ -26,37 +26,78 @@ import java.util.concurrent.TimeUnit
  * Created by Shiplayer on 02.12.18.
  */
 
+/**
+ * Модель, в которой хранится кэш пользователей и с помощью которой можно загрузить посты
+ */
 class WallModel : MainActivity.DestroyListener {
 
+    /**
+     * Класс, в котором сформированны параметры, с помощью которых отправляются запросы на vk api
+     * @param id - айди пользователя или сообщества
+     * @param domain - если не указан [id], то отправляем запрос по которкому имени
+     * @param count - количество записей, которые нужно получить
+     */
     data class RequestData(val id: Int?, val domain: String?, val count: Int?)
-    data class ResponseResult(val list: List<VkWallEntity>, val error: String)
 
-    val userCache = mutableMapOf<Int, User>()
+    /**
+     * Ответ, полученный с vk api и обработанный пробрасывается до [MainActivity] и уже там обрабатывается
+     * @param list - список полученных постов
+     * @param error - сообщение об ошибке, если она возникла, иначе пустая строка
+     * @param total - общее количество записей
+     */
+    data class ResponseResult(var list: List<VkWallEntity>, val error: String, val total: Int)
+
+    /**
+     * Кэш пользователей, чтобы не отправлять каждый раз, чтобы выяснить пользователя по id
+     */
+    private val userCache = mutableMapOf<Int, User>()
+
+    /**
+     * Используется для получения юзеров, если мы не нашли их в нашем кэше
+     */
     private val poolExecutor = Executors.newFixedThreadPool(5)
 
-    private val publish = PublishSubject.create<JsonElement>()
-    private val publicWallMore = PublishSubject.create<JsonElement>()
+    /**
+     * Уведомляет о том, что он получил посты или ошибку при выполнении запроса
+     */
+    private val mPublisher = PublishSubject.create<Notification<JsonElement>>()
+
+    /**
+     * Уведомляет о том, что он смог получить еще посты или ошибку при выполнении запроса для получения еще больше постов
+     */
+    private val mPublishWallMore = PublishSubject.create<Notification<JsonElement>>()
+
+    /**
+     * Используется для отправки запросов на сервер
+     */
     private val publishRequest = PublishSubject.create<RequestData>()
+
+    /**
+     * Компосиция всех disposes, которые нужно будет освободить
+     */
     private val disposable = CompositeDisposable()
 
+    /**
+     * При создании объекта класс, подписывается на получения запросов на получение данных, которые приходят с пользовательского интерфейса
+     */
     init {
         disposable.add(publishRequest
             .throttleFirst(1, TimeUnit.SECONDS)
             .subscribeOn(Schedulers.io())
             .subscribe {
                 val obs = if (it.id != null) {
-                    VkApiService.api.getWall(it.id, count = it.count, accessToken = MainActivity.token)
+                    VkApiService.api.getWall(it.id, count = it.count, accessToken = MainActivity.TOKEN)
                 } else {
-                    VkApiService.api.getWall(domain = it.domain, count = it.count, accessToken = MainActivity.token)
+                    VkApiService.api.getWall(domain = it.domain, count = it.count, accessToken = MainActivity.TOKEN)
                 }
                 obs.subscribeOn(Schedulers.io()).subscribe({ response ->
                     Log.i("WallModel", response.toString())
 /*                    val list = parseResponse(it)
 
-                    publish.onNext(list)*/
-                    publish.onNext(response.body()!!)
+                    mPublisher.onNext(list)*/
+                    mPublisher.onNext(Notification.createOnNext(response.body()!!))
                 }, { error ->
-                    error.printStackTrace()
+                    mPublisher.onNext(Notification.createOnError(error))
                 })
             })
     }
@@ -65,42 +106,60 @@ class WallModel : MainActivity.DestroyListener {
         disposable.add(dispose)
     }
 
-    fun parseResponse(it: JsonElement): ResponseResult {
-        val list = mutableListOf<VkWallEntity>()
-        val calendar = Calendar.getInstance()
-        var error = ""
-        if (it.asJsonObject.has("error")) {
-            error = it.asJsonObject["error"].asJsonObject["error_msg"].asString
-        } else {
-            val response = it.asJsonObject["response"].asJsonObject
 
-            val items = response["items"].asJsonArray
-            parseUser(response)
-            for (item in items) {
-                calendar.timeInMillis = item.asJsonObject["date"].asLong * 1000
-                val time = SimpleDateFormat("dd.MM.yy HH:mm").format(calendar.time)
-                if (item.asJsonObject.has("text")) {
-                    list.add(
-                        VkWallEntity(
-                            item.asJsonObject["from_id"].asInt,
-                            time,
-                            item.asJsonObject["text"].asString
+    /**
+     * Полученный ответ с vk api обрабатывается, проверятеся на наличие ошибок и добавляем юзера (владельца этой страницы) в кэш пользователей
+     * @param it - объект класса [Notification], в котором может содержаться ошибка, которая связанна с соединение
+     * или ответ с сервера, если все прошло успешно
+     * @return Возвращаем результат обработки ответа, обернутого в класс [ResponseResult]
+     */
+    @SuppressLint("SimpleDateFormat")
+    fun parseResponse(it: Notification<JsonElement>): ResponseResult {
+        if (it.isOnNext) {
+            val element = it.value!!
+            val list = mutableListOf<VkWallEntity>()
+            val calendar = Calendar.getInstance()
+            var error = ""
+            var total = -1
+            if (element.asJsonObject.has("error")) {
+                error = element.asJsonObject["error"].asJsonObject["error_msg"].asString
+            } else {
+                val response = element.asJsonObject["response"].asJsonObject
+                total = response["count"].asInt
+                val items = response["items"].asJsonArray
+                parseUser(response)
+                for (item in items) {
+                    calendar.timeInMillis = item.asJsonObject["date"].asLong * 1000
+                    val time = SimpleDateFormat("dd.MM.yy HH:mm").format(calendar.time)
+                    if (item.asJsonObject.has("text")) {
+                        list.add(
+                            VkWallEntity(
+                                item.asJsonObject["from_id"].asInt,
+                                time,
+                                item.asJsonObject["text"].asString
+                            )
                         )
-                    )
-                } else {
-                    list.add(
-                        VkWallEntity(
-                            item.asJsonObject["from_id"].asInt,
-                            time,
-                            ""
+                    } else {
+                        list.add(
+                            VkWallEntity(
+                                item.asJsonObject["from_id"].asInt,
+                                time,
+                                ""
+                            )
                         )
-                    )
+                    }
                 }
             }
+            return ResponseResult(list, error, total)
+        } else {
+            return ResponseResult(listOf(), it.error!!.message!!, -1)
         }
-        return ResponseResult(list, error)
     }
 
+    /**
+     * Обрабатываем пользователя или сообщество и добавляем в кэш пользователей
+     * @param response - [JsonObject] в котом содержится информация об пользователе или сообществе
+     */
     private fun parseUser(response: JsonObject) {
         if (response.has("groups") && response["groups"].asJsonArray.size() > 0) {
             val group = response["groups"].asJsonArray[0].asJsonObject
@@ -126,11 +185,19 @@ class WallModel : MainActivity.DestroyListener {
         }
     }
 
-
+    /**
+     * Если [MainActivity] уничтожается, то освободить местро
+     */
     override fun onDestroy() {
         disposable.dispose()
     }
 
+    /**
+     * Получение пользователя по [id], в случае какой-то ошибки ответ отправится с помощью [listener]
+     * @param id - айди пользователя или сообщества
+     * @param listener - использутеся для отправки уведомления об успешной загрузке или ошибке
+     * @return возвращается пользователь [User]
+     */
     fun getUser(id: Int, listener: UserInteractor): User? {
         if (userCache.containsKey(id)) {
             return userCache[id]
@@ -139,19 +206,28 @@ class WallModel : MainActivity.DestroyListener {
         return null
     }
 
+    /**
+     * Добавляет пользователя в кэщ
+     * @param user - Пользователь, которого необходимо добавить в кэш
+     */
     private fun addUser(user: User) {
         if (!userCache.containsKey(user.id))
             userCache[user.id] = user
     }
 
+    /**
+     * Отправка запроса о получении информации пользователя или сообщества в отдельном потоке.
+     * @param id - айди пользователя или сообщества
+     * @param listener - объект класса [UserInteractor] для обработки полученного ответа с сервера
+     */
     @SuppressLint("CheckResult")
     private fun loadUser(id: Int, listener: UserInteractor) {
         poolExecutor.submit {
             if (id < 0) {
-                VkApiService.api.getGroupInfo((-id).toString(), MainActivity.token).subscribe({
+                VkApiService.api.getGroupInfo((-id).toString(), MainActivity.TOKEN).subscribe({
                     val obj = it.asJsonObject
                     if (obj.has("error")) {
-
+                        listener.onError(Throwable(obj["error_msg"].asString))
                     } else {
                         val info = obj["response"].asJsonArray[0].asJsonObject
                         val user = User(info["name"].asString, "", id, info["photo_100"].asString)
@@ -166,7 +242,7 @@ class WallModel : MainActivity.DestroyListener {
                     listener.onError(it)
                 })
             } else {
-                VkApiService.api.getUserInfo(id.toString(), MainActivity.token).subscribe({
+                VkApiService.api.getUserInfo(id.toString(), MainActivity.TOKEN).subscribe({
                     val body = it.body()!!.asJsonObject["response"].asJsonArray
                     for (item in body) {
                         val itemObject = item.asJsonObject
@@ -191,30 +267,46 @@ class WallModel : MainActivity.DestroyListener {
         }
     }
 
-    fun getWallHandler(): Observable<JsonElement> = publish
+    /**
+     * Получение наблюдателя за получением ответа с сервера
+     */
+    fun getWallHandler(): Observable<Notification<JsonElement>> = mPublisher
 
-    fun getWallMoreHandler(): Observable<JsonElement> = publicWallMore
+    /**
+     * Получение наблюдателя за получением ответа с сервера о получении еще постов
+     */
+    fun getWallMoreHandler(): Observable<Notification<JsonElement>> = mPublishWallMore
 
+    /**
+     * Метод для отправки запроса на получение списка постов для данной страницы
+     * @param idWall - строка, в которой содержится id или короткое имя для данной страницы
+     * @param countWall - строка, в которой содержится количество, которое нужно загрузить
+     */
     fun loadWall(idWall: String, countWall: String) {
         val id = idWall.toIntOrNull()
         val count: Int? = if (countWall.isEmpty()) null else countWall.toInt()
         publishRequest.onNext(RequestData(id, idWall, count))
     }
 
-
+    /**
+     * Метод для отправки запроса на получение следующих постов для данной страницы
+     * @param idWall - строка, в которой содержится id или короткое имя для данной страницы
+     * @param countWall - строка, в которой содержится количество, которое нужно загрузить
+     * @param offset - с какого поста необходиом продолжить
+     */
     fun loadWallMore(idWall: String, countWall: String, offset: Int) {
         val id = idWall.toIntOrNull()
         val count: Int? = if (countWall.isEmpty()) null else countWall.toInt()
         val obs = if (id != null) {
-            VkApiService.api.getWall(id, count = count, offset = offset, accessToken = MainActivity.token)
+            VkApiService.api.getWall(id, count = count, offset = offset, accessToken = MainActivity.TOKEN)
         } else {
-            VkApiService.api.getWall(domain = idWall, count = count, offset = offset, accessToken = MainActivity.token)
+            VkApiService.api.getWall(domain = idWall, count = count, offset = offset, accessToken = MainActivity.TOKEN)
         }
         disposable.add(
             obs.subscribeOn(Schedulers.io()).subscribe({
-                publicWallMore.onNext(it.body()!!)
+                mPublishWallMore.onNext(Notification.createOnNext(it.body()!!))
             }, {
-                publicWallMore.onError(it)
+                mPublishWallMore.onNext(Notification.createOnError(it))
             })
         )
     }
